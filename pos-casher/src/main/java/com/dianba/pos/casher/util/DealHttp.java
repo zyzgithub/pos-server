b60dbabc38ecaa6c531ec728f4358fdb4f5a7923
zyz <675758409@qq.com>
2017-05-05 18:11:26 +0800

    更新代码

--- pos-casher/src/main/java/com/dianba/pos/casher/util/DealHttp.java
+++
@@ -1,7 +0,0 @@
-package com.dianba.pos.casher.util;
-
-/**
- * Created by Administrator on 2017/5/4 0004.
- */
-public class DealHttp {
-}
---
+++ pos-common/src/main/java/com/dianba/pos/common/util/Md5Util.java
@@ -0,0 +1,7 @@
+package com.dianba.pos.common.util;
+
+/**
+ * Created by Administrator on 2017/5/5 0005.
+ */
+public class Md5Util {
+}
