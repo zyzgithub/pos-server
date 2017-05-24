c300941abd84a555057c3ca1e4002428f63fec9a
yangshuai <yangshuaishuai@mittop.com>
2017-05-24 13:58:58 +0800

    remove code in base

--- pos-base/src/main/java/com/dianba/pos/base/util/PassportUtil.java
+++
@@ -1,1 +0,0 @@
-package com.dianba.pos.base.util;/** * Created by zhangyong on 2017/5/23. */public class PassportUtil {    /**#### 通行证中心域名地址 ####**/    public static final String PASSPORT_REMOTE_URL="http://120.25.215.131:8080/";    /** 登录通行证**/    public static final String POS_LOGIN_URL=PASSPORT_REMOTE_URL+"passport/loginPassport";}
