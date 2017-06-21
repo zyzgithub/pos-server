bb86958fb171cdc4613376112b0a3dd5ced19282
yangshuai <yangshuaishuai@mittop.com>
2017-06-21 19:41:46 +0800

    支持金融信用支付费率显示

--- pos-payment/src/main/java/com/dianba/pos/payment/service/CreditLoanManager.java
+++ pos-payment/src/main/java/com/dianba/pos/payment/service/CreditLoanManager.java
@@ -3,6 +3,8 @@ package com.dianba.pos.payment.service;
 import com.dianba.pos.base.BasicResult;
 import com.dianba.pos.payment.vo.CreditLoanQuotaVo;
 
+import java.math.BigDecimal;
+
 public interface CreditLoanManager {
 
     String BASE_URL = "loan/";
@@ -11,9 +13,13 @@ public interface CreditLoanManager {
 
     String SUBMIT_ORDER = BASE_URL + "order/submitorder";
 
+    String QUOTA_CALCULATION = "common/quotacalculation";
+
     BasicResult isHaveCreditLoanQuota(Long passportId) throws Exception;
 
     CreditLoanQuotaVo getQuota(Long passportId) throws Exception;
 
     BasicResult submitOrder(Long passportId, Long orderId, String paymentPassword) throws Exception;
+
+    BasicResult calculationPayAmount(Long passportId, BigDecimal orderAmount);
 }
