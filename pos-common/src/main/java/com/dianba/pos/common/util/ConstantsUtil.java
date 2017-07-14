9006a3914a176ddba600b6f7ea3e481e38dfa1a2
zhangyong <z6543561>
2017-07-14 14:58:28 +0800

    UPDATE JPUSH KEY TO UTIL

--- pos-common/src/main/java/com/dianba/pos/common/util/ConstantsUtil.java
+++ pos-common/src/main/java/com/dianba/pos/common/util/ConstantsUtil.java
@@ -4,7 +4,11 @@ package com.dianba.pos.common.util;
  * Created by zhangyong on 2017/7/10.
  */
 public class ConstantsUtil {
-    public static final String YUN_POS_JIGUANG_SECRET="4619eda3c746333114f26aa6";
-    public static final String YUN_POS_JIGUANG_KEY="10ba6809d6848403d338526d";
+    /**测试**/
+//    public static final String YUN_POS_JIGUANG_SECRET="4619eda3c746333114f26aa6";
+//    public static final String YUN_POS_JIGUANG_KEY="10ba6809d6848403d338526d";
 
+    /**正式**/
+    public static final String YUN_POS_JIGUANG_SECRET="05450c07fa17bf161aeec88c";
+    public static final String YUN_POS_JIGUANG_KEY="07e031d604a9311d8d8bd700";
 }
