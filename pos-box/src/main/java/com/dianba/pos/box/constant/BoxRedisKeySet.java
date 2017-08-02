c03c06b481d3fd241288ed39dfba6b7c7aa5d16b
yangshuai <yangshuaishuai@mittop.com>
2017-08-02 18:33:21 +0800

    修改购买商品缓存

--- pos-box/src/main/java/com/dianba/pos/box/constant/BoxRedisKeySet.java
+++ pos-box/src/main/java/com/dianba/pos/box/constant/BoxRedisKeySet.java
@@ -7,4 +7,6 @@ public class BoxRedisKeySet {
     public static final String DOOR_PLAY_STATUS = BASE_KEY + "DOOR_PLAY_STATUS_";
 
     public static final String DOOR_OPEN_STATUS = BASE_KEY + "DOOR_OPEN_STATUS_";
+
+    public static final String USER_BUY_RFID = BASE_KEY + "USER_BUY_RFID_";
 }
